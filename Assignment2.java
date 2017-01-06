10c1cffa0285ba743a64699905547c095cfe494c
chetan patidar <chetan patidar>
2017-01-07 03:24:34 +0530

    /Write-a-Java-statement-to-assign-the-value-true-to-the-boolean-variable-same-when-the-String-varia

---
+++ Assignment2.java
@@ -0,0 +1,16 @@
+package com.acad.basic;
+
+
+
+public class Assignment2 {
+	public static void main(String[] args) {
+		
+	
+boolean same=true ;
+String s1="chetan";
+String s2= "chetan";
+
+System.out.println(s1==s2);
+
+
+}}
